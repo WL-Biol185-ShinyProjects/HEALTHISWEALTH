4a634041882f4422c072b31a2a5a04d200fe8727
Nayer <fnayer@mail.wlu.edu>
2026-03-23 14:06:46 -0400

    .

--- ui.r
+++ ui.r
@@ -451,7 +451,8 @@ navbarPage("Health is Wealth",
                    )
                )
            )
-  )),
+        )
+    ),
 tabPanel("Data Check Out",
          tags$head(
            tags$style(HTML("
